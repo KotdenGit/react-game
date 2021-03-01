323028e27ddb96af5959e0dd3ef502361dc3592c
KotdenGit <kotden09@gmail.com>
2021-03-01 23:55:40 +0300

    feat: css rule for tiles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import GameField from './components/GameField.js';
 
@@ -7,18 +7,7 @@ function App() {
     <div className="App">
       <GameField />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
--- src/components/GameField.js
+++ src/components/GameField.js
@@ -1,12 +1,14 @@
 import {gameTiles} from "./FieldTiles.js";
+import './GameField.css';
 
 function GameField() {
      
     // console.log(gameTiles);
-    const CreatedField = gameTiles.map((item, i) => (<img key={i} src={item.src}/>));
+    const CreatedField = gameTiles.map((item, i) => (<img key={i} src={item.src} className="Game-tile-items"/>));
     console.log(CreatedField);
+    
     return (
-        <div className="Title">
+        <div className="Game-tile">
           {CreatedField}
         </div>
       );
