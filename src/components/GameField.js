1067ceb9c3d52ca516f4b1c7ef63b68c8635536d
KotdenGit <kotden09@gmail.com>
2021-03-03 20:30:31 +0300

    feat: implement handle-click

--- src/components/GameField.js
+++ src/components/GameField.js
@@ -1,17 +1,37 @@
+import React, { useState } from "react";
 import {gameTiles} from "./FieldTiles.js";
 import './GameField.css';
 
 function GameField() {
-     
-    // console.log(gameTiles);
-    const CreatedField = gameTiles.map((item, i) => (<img key={i} src={item.src} className="Game-tile-items"/>));
-    console.log(CreatedField);
+
+  const ShuffleTiles = shuffle(gameTiles);
+  
+  const [tiles, setTiles] = useState([...ShuffleTiles]);
+  
+  let contRaund = 1;
+  
+  function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+  
     
-    return (
-        <div className="Game-tile">
-          {CreatedField}
-        </div>
-      );
+  function handleClick(position, title) {
+    console.log(position, title);
+  }
+
+  return (
+      <div className="Game-tile">
+        {tiles.map((item, i) => (
+        <img onClick={() => handleClick(item, i)} key={i} 
+        src={item.src} alt={item.features + item.plants} 
+        className="Game-tile-item"/>))}
+        
+      </div>
+    );
     
   }
   
